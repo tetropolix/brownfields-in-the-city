3716676abe09bc6108c064efca14e8deaf3d62c3
Peter Grocký <peter.grocky@student.tuke.sk>
2022-11-04 17:24:19 +0100

    basic login form

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,6 +1,62 @@
+import {Box, Button, Container, CssBaseline, TextField, Typography} from "@mui/material";
+
 function Login() {
+
     return (
-        <h1>Login Page</h1>
+        <>
+            <CssBaseline />
+            <Container maxWidth={"xs"}>
+
+                <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        mt: 12,
+                        p: 4,
+                        border: '1px solid #C0C0C0',
+                        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+                        borderRadius: '12px'
+                    }}
+                >
+                    <Typography component="h1" variant="h4" textAlign="center">
+                        Prihlásenie
+                    </Typography>
+
+                    <Box component="form"  noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Heslo"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                           Prihlásiť sa
+                        </Button>
+                    </Box>
+                </Box>
+
+
+
+            </Container>
+        </>
     )
 }
 
