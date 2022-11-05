9f0d0f28407d9cf012e55dbda7a327727ba9053c
Peter Grocký <peter.grocky@student.tuke.sk>
2022-11-05 16:52:01 +0100

    global theme for mui added to _app.js

--- frontend/src/components/LoginLayout/LoginLayout.js
+++ frontend/src/components/LoginLayout/LoginLayout.js
@@ -1,25 +1,13 @@
-import {Container, CssBaseline, ThemeProvider} from "@mui/material";
-import {createTheme} from "@mui/material/styles";
-
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#747ab7'
-        }
-    },
-});
+import {Container, CssBaseline} from "@mui/material";
 
 function LoginLayout({ children }) {
     return (
-        <ThemeProvider theme={theme}>
-            <Container maxWidth="xxl" sx={{display: "flex", background: "linear-gradient(120grad, #e66465, #9198e5);", height: "100vh", p: 0}}>
-                <CssBaseline />
-                <Container maxWidth="xs" sx={{display: "flex", alignItems: 'center'}}>
-                    {children}
-                </Container>
+        <Container maxWidth="xxl" sx={{display: "flex", background: "linear-gradient(120grad, #e66465, #9198e5);", height: "100vh", p: 0}}>
+            <CssBaseline />
+            <Container maxWidth="xs" sx={{display: "flex", alignItems: 'center'}}>
+                {children}
             </Container>
-        </ThemeProvider>
+        </Container>
     )
 }
 
--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import {createTheme} from "@mui/material/styles";
+import {ThemeProvider} from "@mui/material";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#747ab7'
+      }
+    },
+  });
+
+  return (
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+  )
 }
 
 export default MyApp
