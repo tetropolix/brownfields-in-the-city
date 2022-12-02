87b3fb6243885683e6e1aac192af050212c904f6
Ján Gomboš <jan.gombos@student.tuke.sk>
2022-12-02 18:18:59 +0100

    added cors origin for localhost dev

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,9 +4,22 @@ from globals import EnvVars
 from routers import brownfields_router, auth_router
 from fastapi_pagination import add_pagination
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3002",
+    "http://localhost:*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_router.router)
 app.include_router(brownfields_router.router)
 app.mount(
