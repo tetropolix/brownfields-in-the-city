eebf640b40829b1b1da99c1c26c1357d84ceeb53
Ján Gomboš <jan.gombos@student.tuke.sk>
2023-01-10 10:45:19 +0100

    populate script for mockup brownfields using http

--- backend/testing/populate.py
+++ backend/testing/populate.py
@@ -2,12 +2,14 @@ import base64
 import json
 import os
 import random
+import time
 import requests  # type: ignore
 from urllib3 import encode_multipart_formdata
 from urllib3.fields import RequestField
 
 
 api_url = "http://localhost:8000/brownfields/insert"
+time_between_requests = 0.1 #in sec
 
 
 def load_mockups() -> list:
@@ -27,19 +29,18 @@ def read_files() -> list:
 def insert_brownfield(api_url, data: str, files: list):
     fields = [("data", data)] + files
     body, header = encode_multipart_formdata(fields)
-    response = requests.post(api_url, headers={"Content-Type": header}, data=body)
-    if response.status_code > 299:
-        exit(1)
-    print("OK - " + str(response.json()))
+    return requests.post(api_url, headers={"Content-Type": header}, data=body)
 
 
 if __name__ == "__main__":
     files = read_files()
-    print(len(files))
     for row in load_mockups():
-        insert_brownfield(
+        response = insert_brownfield(
             api_url,
             json.dumps(row),
             random.sample(files, random.randint(0, len(files))),
         )
-        exit(0)
+        if response.status_code > 299:
+            exit(1)
+        print("OK - " + str(response.json()))
+        time.sleep(time_between_requests)
